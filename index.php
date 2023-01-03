6eec398680ee57daecbd577e1eec986895909f01
shaikhfaizanhassan <95478008+shaikhfaizanhassan@users.noreply.github.com>
2023-01-03 03:19:39 -0800

    file

---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Dashboard</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="./assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="./assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="assets/css/main.min.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+</head>
+
+<body class="fixed-navbar">
+    <div class="page-wrapper">
+        <!-- START HEADER-->
+        <header class="header">
+            <div class="page-brand">
+                <a class="link" href="index.html">
+                    <span class="brand">TND
+
+                    </span>
+
+                </a>
+            </div>
+            <div class="flexbox flex-1">
+                <!-- START TOP-LEFT TOOLBAR-->
+                <ul class="nav navbar-toolbar">
+                    <li>
+                        <a class="nav-link sidebar-toggler js-sidebar-toggler"><i class="ti-menu"></i></a>
+                    </li>
+                    <li>
+                        <form class="navbar-search" action="javascript:;">
+                            <div class="rel">
+                                <span class="search-icon"><i class="ti-search"></i></span>
+                                <input class="form-control" placeholder="Search here...">
+                            </div>
+                        </form>
+                    </li>
+                </ul>
+                <!-- END TOP-LEFT TOOLBAR-->
+                <!-- START TOP-RIGHT TOOLBAR-->
+                <ul class="nav navbar-toolbar">
+                    <li class="dropdown dropdown-inbox">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope-o"></i>
+                            <span class="badge badge-primary envelope-badge">9</span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-right dropdown-menu-media">
+                            <li class="dropdown-menu-header">
+                                <div>
+                                    <span><strong>9 New</strong> Messages</span>
+                                    <a class="pull-right" href="mailbox.html">view all</a>
+                                </div>
+                            </li>
+                            <li class="list-group list-group-divider scroller" data-height="240px" data-color="#71808f">
+                                <div>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <img src="./assets/img/users/u1.jpg" />
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-strong"> </div>Jeanne Gonzalez<small class="text-muted float-right">Just now</small>
+                                                <div class="font-13">Your proposal interested me.</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <img src="./assets/img/users/u2.jpg" />
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-strong"></div>Becky Brooks<small class="text-muted float-right">18 mins</small>
+                                                <div class="font-13">Lorem Ipsum is simply.</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <img src="./assets/img/users/u3.jpg" />
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-strong"></div>Frank Cruz<small class="text-muted float-right">18 mins</small>
+                                                <div class="font-13">Lorem Ipsum is simply.</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <img src="./assets/img/users/u4.jpg" />
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-strong"></div>Rose Pearson<small class="text-muted float-right">3 hrs</small>
+                                                <div class="font-13">Lorem Ipsum is simply.</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown dropdown-notification">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell-o rel"><span class="notify-signal"></span></i></a>
+                        <ul class="dropdown-menu dropdown-menu-right dropdown-menu-media">
+                            <li class="dropdown-menu-header">
+                                <div>
+                                    <span><strong>5 New</strong> Notifications</span>
+                                    <a class="pull-right" href="javascript:;">view all</a>
+                                </div>
+                            </li>
+                            <li class="list-group list-group-divider scroller" data-height="240px" data-color="#71808f">
+                                <div>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <span class="badge badge-success badge-big"><i class="fa fa-check"></i></span>
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-13">4 task compiled</div><small class="text-muted">22 mins</small>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <span class="badge badge-default badge-big"><i class="fa fa-shopping-basket"></i></span>
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-13">You have 12 new orders</div><small class="text-muted">40 mins</small>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <span class="badge badge-danger badge-big"><i class="fa fa-bolt"></i></span>
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-13">Server #7 rebooted</div><small class="text-muted">2 hrs</small>
+                                            </div>
+                                        </div>
+                                    </a>
+                                    <a class="list-group-item">
+                                        <div class="media">
+                                            <div class="media-img">
+                                                <span class="badge badge-success badge-big"><i class="fa fa-user"></i></span>
+                                            </div>
+                                            <div class="media-body">
+                                                <div class="font-13">New user registered</div><small class="text-muted">2 hrs</small>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown dropdown-user">
+                        <a class="nav-link dropdown-toggle link" data-toggle="dropdown">
+                            <img src="./assets/img/admin-avatar.png" />
+                            <span></span>Admin<i class="fa fa-angle-down m-l-5"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item" href="profile.html"><i class="fa fa-user"></i>Profile</a>
+                            <a class="dropdown-item" href="profile.html"><i class="fa fa-cog"></i>Settings</a>
+                            <a class="dropdown-item" href="javascript:;"><i class="fa fa-support"></i>Support</a>
+                            <li class="dropdown-divider"></li>
+                            <a class="dropdown-item" href="login.html"><i class="fa fa-power-off"></i>Logout</a>
+                        </ul>
+                    </li>
+                </ul>
+                <!-- END TOP-RIGHT TOOLBAR-->
+            </div>
+        </header>
+        <!-- END HEADER-->
+        <!-- START SIDEBAR-->
+        <nav class="page-sidebar" id="sidebar">
+            <div id="sidebar-collapse">
+                <div class="admin-block d-flex">
+                    <div>
+                        <img src="./assets/img/admin-avatar.png" width="45px" />
+                    </div>
+                    <div class="admin-info">
+                        <div class="font-strong">TND</div><small>Administrator</small>
+                    </div>
+                </div>
+                <ul class="side-menu metismenu">
+                    <li>
+                        <a class="active" href="index.html"><i class="sidebar-item-icon fa fa-th-large"></i>
+                            <span class="nav-label">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="heading">FEATURES</li>
+                    <li>
+                        <a href="javascript:;"><i class="sidebar-item-icon fa fa-bookmark"></i>
+                            <span class="nav-label">Requsition</span><i class="fa fa-angle-left arrow"></i></a>
+                        <ul class="nav-2-level collapse">
+                            <li>
+                                <a href="index.php?uploadRequision">Upload Requsition</a>
+                            </li>
+                            <li>
+                                <a href="colors.html">View Requsition</a>
+                            </li>
+                            <li>
+                                <a href="colors.html">Batch Wise</a>
+                            </li>
+                            <li>
+                                <a href="colors.html">Student Wise</a>
+                            </li>
+                            <li>
+                                <a href="colors.html">Teacher Wise</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="sidebar-item-icon fa fa-bookmark"></i>
+                            <span class="nav-label">Control File</span><i class="fa fa-angle-left arrow"></i></a>
+                        <ul class="nav-2-level collapse">
+                            <li>
+                                <a href="colors.html">View control Sheet</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="icons.html"><i class="sidebar-item-icon fa fa-smile-o"></i>
+                            <span class="nav-label">Icons</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- END SIDEBAR-->
+        <div class="content-wrapper">
+            <!-- START PAGE CONTENT-->
+            <div class="page-content fade-in-up">
+                <div class="row">
+                   <?php 
+                    if(isset($_GET["uploadRequision"]))
+                    {
+                        include("uploadRequision.php");
+                    }
+                    else
+                    {
+                       echo '
+                       <div class="col-lg-3 col-md-6">
+                       <div class="ibox bg-success color-white widget-stat">
+                           <div class="ibox-body">
+                               <h2 class="m-b-5 font-strong">201</h2>
+                               <div class="m-b-5">NEW ORDERS</div><i class="ti-shopping-cart widget-stat-icon"></i>
+                               <div><i class="fa fa-level-up m-r-5"></i><small>25% higher</small></div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-lg-3 col-md-6">
+                       <div class="ibox bg-info color-white widget-stat">
+                           <div class="ibox-body">
+                               <h2 class="m-b-5 font-strong">1250</h2>
+                               <div class="m-b-5">UNIQUE VIEWS</div><i class="ti-bar-chart widget-stat-icon"></i>
+                               <div><i class="fa fa-level-up m-r-5"></i><small>17% higher</small></div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-lg-3 col-md-6">
+                       <div class="ibox bg-warning color-white widget-stat">
+                           <div class="ibox-body">
+                               <h2 class="m-b-5 font-strong">$1570</h2>
+                               <div class="m-b-5">TOTAL INCOME</div><i class="fa fa-money widget-stat-icon"></i>
+                               <div><i class="fa fa-level-up m-r-5"></i><small>22% higher</small></div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-lg-3 col-md-6">
+                       <div class="ibox bg-danger color-white widget-stat">
+                           <div class="ibox-body">
+                               <h2 class="m-b-5 font-strong">108</h2>
+                               <div class="m-b-5">NEW USERS</div><i class="ti-user widget-stat-icon"></i>
+                               <div><i class="fa fa-level-down m-r-5"></i><small>-12% Lower</small></div>
+                           </div>
+                       </div>
+                   </div>
+            
+                       ';
+                    }
+                   ?>
+             
+             
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+        <!-- END PAGE CONTENT-->
+
+    </div>
+    </div>
+    <!-- BEGIN THEME CONFIG PANEL-->
+    <!-- END THEME CONFIG PANEL-->
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div>
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS-->
+    <script src="./assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+    <script src="./assets/vendors/chart.js/dist/Chart.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
+    <script src="./assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+    <script src="./assets/vendors/jvectormap/jquery-jvectormap-us-aea-en.js" type="text/javascript"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="assets/js/app.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="./assets/js/scripts/dashboard_1_demo.js" type="text/javascript"></script>
+</body>
+
+</html>
---
+++ uploadRequision.php
@@ -0,0 +1,38 @@
+<div class="container">
+
+
+<div class="page-content fade-in-up">
+    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="ibox">
+                <div class="ibox-head">
+                    <div class="ibox-title">Upload Requsition</div>
+                    <div class="ibox-tools">
+                        <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
+                        <a class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></a>
+                        
+                    </div>
+                </div>
+                <div class="ibox-body">
+                    <form>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Upload Requsition</label>
+                                <input class="form-control" type="file" placeholder="First Name">
+                            </div>
+                            
+                        </div>
+                      
+                        
+                     
+                        <div class="form-group">
+                            <button class="btn btn-primary" type="submit">Upload</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
